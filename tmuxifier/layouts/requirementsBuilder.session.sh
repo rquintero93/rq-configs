21e8b942a4c6e7f6a3ccd3198e0cbeadcf51062b
Ricardo Quintero <rquinterog93@gmail.com>
2025-04-05 18:07:45 +0200

    update conf files

---
+++ tmuxifier/layouts/requirementsBuilder.session.sh
@@ -0,0 +1,10 @@
+# Layout for requirementsBuilder
+session_root "/Users/ricardoquintero/Documents/Global Alumni/requirementsBuilder"
+
+if initialize_session "requirementsBuilder"; then
+  load_window "nvim"
+  load_window "shell"
+  select_window 1
+fi
+
+finalize_and_go_to_session
