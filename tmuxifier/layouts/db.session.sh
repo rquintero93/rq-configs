a6f80b9a3f2514e3e9e86459da2d146d001243c2
Ricardo Quintero <rquinterog93@gmail.com>
2025-04-07 12:50:54 +0200

    db tmux session

---
+++ tmuxifier/layouts/db.session.sh
@@ -0,0 +1,21 @@
+# Set a custom session root path. Default is `$HOME`.
+# Must be called before `initialize_session`.
+session_root "~/Documents/Global Alumni/Queries/"
+
+# Create session with specified name if it does not already exist. If no
+# argument is given, session name will be based on layout file name.
+if initialize_session "db"; then
+
+  # Load a defined window layout.
+  load_window "nvim"
+
+  new_window "shell"
+
+  # Select the default active window on session creation.
+
+  select_window 1
+
+fi
+
+# Finalize session creation and switch/attach to it.
+finalize_and_go_to_session
