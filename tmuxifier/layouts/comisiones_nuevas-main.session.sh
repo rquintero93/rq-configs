cbd4b1a202460812334b8a37d8f808a6d5602483
Ricardo Quintero <rquinterog93@gmail.com>
2025-04-09 21:49:40 +0200

    config updates

---
+++ tmuxifier/layouts/comisiones_nuevas-main.session.sh
@@ -0,0 +1,10 @@
+# Layout for comisiones_nuevas-main
+session_root "/Users/ricardoquintero/Documents/Global Alumni/comisiones_nuevas-main"
+
+if initialize_session "comisiones_nuevas-main"; then
+  load_window "nvim"
+  load_window "shell"
+  select_window 1
+fi
+
+finalize_and_go_to_session
