cbba8a629e98dfe476e844894e719238ca886f29
Ricardo Quintero <rquinterog93@gmail.com>
2025-04-10 18:34:39 +0200

    lazy git and docker aliases

---
+++ tmuxifier/layouts/santanderCOCS.session.sh
@@ -0,0 +1,10 @@
+# Layout for santanderCOCS
+session_root "/Users/ricardoquintero/Documents/Global Alumni/santanderCOCS"
+
+if initialize_session "santanderCOCS"; then
+  load_window "nvim"
+  load_window "shell"
+  select_window 1
+fi
+
+finalize_and_go_to_session
